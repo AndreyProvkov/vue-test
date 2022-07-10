b859aae18b19547fb31be00fba62ba0ef2a086fc
Fen1xx <provkovasu15@gmail.com>
2022-07-10 16:22:09 +0500

    Add content to card-item

---
+++ components/ItemCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="item">
+    <div class="item__image">
+      <img src="/camera.jpg" alt="camera" />
+    </div>
+    <h2>Наименование товара</h2>
+    <p class="item__description">
+      Довольно-таки интересное описание товара в несколько строк. Довольно-таки
+      интересное описание товара в несколько строк
+    </p>
+    <div class="item__price"></div>
+    <button class="btn-remove item__remove">
+      <BtnRemove />
+    </button>
+  </div>
+</template>
+
+<script>
+import BtnRemove from './btn-remove.vue'
+export default {
+  components: { BtnRemove }
+}
+</script>
