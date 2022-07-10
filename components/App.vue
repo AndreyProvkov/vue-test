36395f3d90df99427fa59472e881bb5638bb0332
Fen1xx <provkovasu15@gmail.com>
2022-07-10 21:50:39 +0500

    Add style to container

--- components/App.vue
+++ components/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <h1>Hello</h1>
+  <div class="container">
+    <HeaderPage class="header-block" />
+    <AddFormItem class="form-block" />
+    <ItemList class="list-block" />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {}
+  name: 'App'
 }
 </script>
+
+<style scoped lang='scss'>
+  .container {
+    padding: 3.2rem;
+    padding-bottom: 0;
+    max-width: 137.6rem;
+    margin: 0 auto;
+  }
+</style>
