711d255df927ee5f0f6dcde21962c91c23a68ae6
Fen1xx <provkovasu15@gmail.com>
2022-07-10 18:29:02 +0500

    Add styles to item card

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <Nuxt />
+</template>
+
+<style lang='scss'>
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap');
+
+  html {
+    font-size: 10px;
+  }
+
+  body {
+      margin: 0;
+      font-family: 'Source Sans Pro', sans-serif;
+  }
+</style>
