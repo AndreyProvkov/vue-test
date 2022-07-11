808086524137a4123b3bd92976398addf787557c
Fen1xx <provkovasu15@gmail.com>
2022-07-11 13:01:35 +0500

    Add global styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,7 @@
 
 <style lang='scss'>
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap');
 
   html {
     font-size: 10px;
@@ -12,5 +13,6 @@
   body {
       margin: 0;
       font-family: 'Source Sans Pro', sans-serif;
+      background: rgba(255, 254, 251, 0.8);
   }
 </style>
